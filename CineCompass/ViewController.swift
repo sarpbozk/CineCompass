8d61c4a15174f9d43b56ea1dd3bf26fb732e43c4
Muhammet Bozkurt <mubozkt@gmail.com>
2023-04-19 03:15:52 +0300

    Initial Commit

---
+++ CineCompass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineCompass
+//
+//  Created by Muhammet BOZKURT on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
