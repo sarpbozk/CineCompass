6116879e7ef0184ed74925f06bb87e01dbe76dc2
Muhammet Bozkurt <mubozk@icloud.com>
2023-04-27 09:44:39 +0300

    display movie posters on movie details
    MovieDetailsViewController movie poster images using Kingfisher.

--- CineCompass/Controller/MovieDetailsViewController.swift
+++ CineCompass/Controller/MovieDetailsViewController.swift
@@ -23,10 +23,10 @@ class MovieDetailsViewController: UIViewController {
         movieGenres.text = (movieDetails?.genres[0].name ?? " ") + "   " + (movieDetails?.genres[1].name ?? " ") + "   " + (movieDetails?.genres[2].name ?? " ")
         movieOverview.text = movieDetails?.overview
         
-//        let baseURL = "https://image.tmdb.org/t/p/w500/"
-//        if let posterPath = movieDetails?.posterPath {
-//            let posterURL = URL(string: baseURL + posterPath)
-//            moviePoster.kf.setImage(with: posterURL)
-//        }
+        let baseURL = "https://image.tmdb.org/t/p/w500/"
+        if let posterPath = movieDetails?.posterPath {
+            let posterURL = URL(string: baseURL + posterPath)
+            moviePoster.kf.setImage(with: posterURL)
+        }
     }
 }
